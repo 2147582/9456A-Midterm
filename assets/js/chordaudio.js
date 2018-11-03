4b16007c2c709159c0107c0aecf5aa523425b415
Justine Paul Ballares <2160138@slu.edu.ph>
2018-11-03 14:54:25 +0800

    added autoplay for quiz audio

--- assets/js/chordaudio.js
+++ assets/js/chordaudio.js
@@ -4,11 +4,24 @@ function play(chord) {
     audio.play();
 }
 
-$(document).ready(function () {
-    $('.chord-audio').each(function () {
-        var chord = $(this).attr('chord');
-        var htmlContent = '<audio chord="' + chord + '" src="assets/audio/' + chord + '.AAC"></audio>';
-        htmlContent += '<a class="btn btn-primary" onclick="play(\'' + chord + '\')">Play</a>';
-        $(this).html(htmlContent);
+function chordAudio() {
+    $(document).ready(function () {
+        $('.chord-audio').each(function () {
+            var chord = $(this).attr('chord');
+            var htmlContent = '<audio chord="' + chord + '" src="assets/audio/' + chord + '.AAC"></audio>';
+            htmlContent += '<a class="btn btn-primary" onclick="play(\'' + chord + '\')">Play</a>';
+            $(this).html(htmlContent);
+        });
     });
-});
+}
+
+function chordAudioQuiz() {
+    $(document).ready(function () {
+        $('.chord-audio').each(function () {
+            var chord = $(this).attr('chord');
+            var htmlContent = '<audio chord="' + chord + '" src="assets/audio/' + chord + '.AAC" autoplay></audio>';
+            htmlContent += '<a class="btn btn-primary" onclick="play(\'' + chord + '\')">Play</a>';
+            $(this).html(htmlContent);
+        });
+    });
+}
