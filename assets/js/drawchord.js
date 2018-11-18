d433bb345848f0eb328d0abcf3eef1bb7d45da6a
Justine Paul Ballares <2160138@slu.edu.ph>
2018-11-18 13:45:19 +0800

    fix some parts of drawing of chords

--- assets/js/drawchord.js
+++ assets/js/drawchord.js
@@ -3,7 +3,7 @@ var stringWidth = 20;
 var textOffsetY = -3;
 
 function removeX(array) {
-    var searchTerm = 'x';
+    var searchTerm = 'X';
     cleanArray = [];
 
     for (n = 0; n <= array.length - 1; n++) {
@@ -28,6 +28,7 @@ function initChords() {
 }
 
 function drawChord(chordName, canvas) {
+    chordName = chordName.replace(/#/g, '%23');
     var c = canvas;
     var ctx = c.getContext('2d');
 
@@ -43,89 +44,97 @@ function drawChord(chordName, canvas) {
         var cs = chord[0].strings;
 
         chordStrings = cs.split(' ').map(function (item) {
-            return parseInt(item, 10);
+            if (item != "X") {
+                return parseInt(item, 10);
+            } else {
+                return "X";
+            }
+
         });
 
         var f = chord[0].fingering;
 
         chordFingering = f.split(' ').map(function (item) {
-            return parseInt(item, 10);
+            if (item != "X") {
+                return parseInt(item, 10);
+            } else {
+                return "X";
+            }
         });
-
+        
         fret = Math.min.apply(Math, removeX(chordStrings));
+        var offsetY = 20;
+        var offsetX = 0;
+
+        var maxPos = Math.max.apply(Math, removeX(chordStrings)) + 1;
+        var minPos = Math.min.apply(Math, removeX(chordStrings));
+
+        for (f = minPos; f <= maxPos + 1; f++) {
+            ctx.beginPath();
+
+            if (f == minPos && fret == 0) {
+                ctx.lineWidth = 4;
+            } else {
+                ctx.lineWidth = 1;
+            }
+            //draw the frets
+            ctx.moveTo(0 + stringWidth, offsetY + fretHeight);
+            ctx.lineTo(100 + stringWidth, offsetY + fretHeight);
+            ctx.strokeStyle = 'gray';
+            ctx.stroke();
+
+            var offsetY = offsetY + 20;
+        }
+        for (i = 0; i < chordStrings.length; i++) {
+            var stringH = (maxPos - minPos + 1) * fretHeight;
+
+            if (chordStrings[i] == 'X') {
+                var y = fretHeight + textOffsetY;
+            } else {
+                var y = (fretHeight * chordStrings[i]) + fretHeight + textOffsetY;
+            }
+
+            var x = stringWidth + offsetX - (stringWidth / 4);
+
+            //draw the strings
+            ctx.beginPath();
+            ctx.fillStyle = 'white';
+            ctx.fillRect(x, y - 10, 10, 10);
+
+            ctx.moveTo(offsetX + stringWidth, 20 + fretHeight);
+            ctx.lineTo(offsetX + stringWidth, 20 + stringH + fretHeight);
+
+            ctx.fillStyle = 'black';
+            if (fret != 0 && chordFingering[i] != 0) {
+                ctx.fillText(chordFingering[i], x, y + 20);
+            } else {
+                ctx.fillText(chordFingering[i], x, y + 20);
+            }
+
+            ctx.stroke();
+
+            if (i == chordStrings.length - 1 && fret != 0) {
+
+                ctx.fillText(fret + ' fret', x + 20, y + 20);
+            }
+            var offsetX = offsetX + stringWidth;
+        }
+        displayName = chord[0].chordName.replace(/,/g, '');
+        ctx.fillText(displayName, 63, (maxPos - minPos + 4) * fretHeight);
+        ctx.font = "14px Arial";
+        ctx.fillText('E', stringWidth - 5, 15);
+        ctx.fillText('A', (stringWidth * 2) - 5, 15);
+        ctx.fillText('D', (stringWidth * 3) - 5, 15);
+        ctx.fillText('G', (stringWidth * 4) - 5, 15);
+        ctx.fillText('B', (stringWidth * 5) - 5, 15);
+        ctx.fillText('E', (stringWidth * 6) - 5, 15);
+
+        ctx.font = "18px Arial";
     });
 
 
 
-    var offsetY = 20;
-    var offsetX = 0;
 
-    var maxPos = Math.max.apply(Math, removeX(chordStrings)) + 1;
-    var minPos = Math.min.apply(Math, removeX(chordStrings));
-    console.log(chord.strings);
-
-    for (f = minPos; f <= maxPos + 1; f++) {
-        ctx.beginPath();
-
-        if (f == minPos && fret == 0) {
-            ctx.lineWidth = 4;
-        } else {
-            ctx.lineWidth = 1;
-        }
-        //draw the frets
-        ctx.moveTo(0 + stringWidth, offsetY + fretHeight);
-        ctx.lineTo(100 + stringWidth, offsetY + fretHeight);
-        ctx.strokeStyle = 'gray';
-        ctx.stroke();
-
-        var offsetY = offsetY + 20;
-    }
-
-    for (i = 0; i < chordStrings; i++) {
-        var stringH = (maxPos - minPos + 1) * fretHeight;
-
-        if (chord.placement[i] == 'x') {
-            var y = fretHeight + textOffsetY;
-        } else {
-            var y = (fretHeight * chordStrings[i]) + fretHeight + textOffsetY;
-        }
-
-        var x = stringWidth + offsetX - (stringWidth / 4);
-
-        //draw the strings
-        ctx.beginPath();
-        ctx.fillStyle = 'white';
-        ctx.fillRect(x, y - 10, 10, 10);
-
-        ctx.moveTo(offsetX + stringWidth, 20 + fretHeight);
-        ctx.lineTo(offsetX + stringWidth, 20 + stringH + fretHeight);
-
-        ctx.fillStyle = 'black';
-        if (chord.fret != 0 && chordFingering[i] != 0) {
-            ctx.fillText(chordFingering[i], x, y + 20);
-        } else {
-            ctx.fillText(chordFingering, x, y + 20);
-        }
-
-        ctx.stroke();
-
-        if (i == chordStrings.length - 1 && chord.fret != 0) {
-
-            ctx.fillText(chord.fret + ' fret', x + 20, y + 20);
-        }
-        var offsetX = offsetX + stringWidth;
-    }
-    displayName = chord[0].chordName.replace(',', '');
-    ctx.fillText(displayName, 63, (maxPos - minPos + 4) * fretHeight);
-    ctx.font = "14px Arial";
-    ctx.fillText('E', stringWidth - 5, 15);
-    ctx.fillText('A', (stringWidth * 2) - 5, 15);
-    ctx.fillText('D', (stringWidth * 3) - 5, 15);
-    ctx.fillText('G', (stringWidth * 4) - 5, 15);
-    ctx.fillText('B', (stringWidth * 5) - 5, 15);
-    ctx.fillText('E', (stringWidth * 6) - 5, 15);
-
-    ctx.font = "18px Arial";
 }
 
 //not yet modified
